13a86db6adce515f75928dac792985936e610d7c
“ravinrawat” <ravinrawat.wd@gmail.com>
2024-04-13 19:38:22 -0600

    Added initial styles

--- navigators/RootNavigator.js
+++ navigators/RootNavigator.js
@@ -1,4 +1,5 @@
 import * as React from "react";
+import { SafeAreaView, StyleSheet, useColorScheme } from "react-native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import WelcomeScreen from "../screens/WelcomeScreen";
 import SubscribeScreen from "../screens/SubscribeScreen";
@@ -6,12 +7,26 @@ import SubscribeScreen from "../screens/SubscribeScreen";
 const { Navigator, Screen } = createNativeStackNavigator();
 
 const RootNavigator = () => {
+  const colorScheme = useColorScheme();
   return (
-    <Navigator>
-      <Screen name="welcome" component={WelcomeScreen} />
-      <Screen name="subscribe" component={SubscribeScreen} />
-    </Navigator>
+    <SafeAreaView
+      style={[
+        styles.container,
+        { backgroundColor: colorScheme === "light" ? "#FFF" : "#333333" },
+      ]}
+    >
+      <Navigator>
+        <Screen name="welcome" component={WelcomeScreen} />
+        <Screen name="subscribe" component={SubscribeScreen} />
+      </Navigator>
+    </SafeAreaView>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
 
 export default RootNavigator;
--- screens/WelcomeScreen.js
+++ screens/WelcomeScreen.js
@@ -1,9 +1,57 @@
-import * as React from 'react';
-import { View } from 'react-native';
+import * as React from "react";
+import { Text, View, Image, StyleSheet, Pressable } from "react-native";
 
 const WelcomeScreen = ({ navigation }) => {
   // Add welcome screen code here.
-  return <View></View>;
+  return (
+    <View style={styles.container}>
+      <Image
+        style={styles.logo}
+        source={require("../assets/little-lemon-logo.png")}
+      />
+      <Text style={styles.subheading}>
+        Little Lemon, your local Mediterranean Bistro
+      </Text>
+      <Pressable style={styles.button}>
+        <Text>Newsletter</Text>
+      </Pressable>
+    </View>
+  );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "space-evenly",
+    alignItems: "center",
+    borderWidth: 2,
+    borderColor: "red",
+  },
+  logo: {
+    // alignSelf: "center",
+    width: 150,
+    height: 350,
+    resizeMode: "contain",
+    borderWidth: 2,
+    borderColor: "red",
+  },
+  subheading: {
+    width: "90%",
+    justifyContent: "center",
+    alignContent: "center",
+    textAlign: "center",
+    fontSize: 24,
+    borderWidth: 2,
+    borderColor: "red",
+  },
+  button: {
+    height: 50,
+    width: 200,
+    justifyContent: "center",
+    alignItems: "center",
+    borderWidth: 2,
+    borderColor: "red",
+  },
+});
 
 export default WelcomeScreen;
